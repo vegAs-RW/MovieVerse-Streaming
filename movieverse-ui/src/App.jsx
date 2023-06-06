4d9407500aef25abea6e5265c7c3f0875aae1c68
Jordan <jordan.pouchot2@gmail.com>
2023-06-06 16:40:07 +0200

    add new route for player

--- movieverse-ui/src/App.jsx
+++ movieverse-ui/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from "./pages/Login";
 import Moviverse from "./pages/Moviverse";
+import Player from "./pages/Player";
 import Signup from "./pages/Signup";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/player" element={<Player />} />
         <Route exact path="/" element={<Moviverse />} />
       </Routes>
     </BrowserRouter>
